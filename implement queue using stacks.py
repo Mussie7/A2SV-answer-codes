ddeeef7b82e26297d67fe7c419b37dc23f398ccd
Mussie <90728686+Mussie7@users.noreply.github.com>
2022-04-13 12:25:49 +0300

    Create implement queue using stacks.py

---
+++ implement queue using stacks.py
@@ -0,0 +1,38 @@
+class MyQueue:
+
+    def __init__(self):
+        self.__stack1 = []
+        self.__stack2 = []
+
+    def push(self, x: int) -> None:
+        self.__stack1.append(x)
+
+    def pop(self) -> int:
+        while len(self.__stack1) > 0:
+            self.__stack2.append(self.__stack1.pop())
+        first = self.__stack2.pop()
+        while len(self.__stack2) > 0:
+            self.__stack1.append(self.__stack2.pop())
+        return first
+
+    def peek(self) -> int:
+        while len(self.__stack1) > 0:
+            self.__stack2.append(self.__stack1.pop())
+        first = self.__stack2[-1]
+        while len(self.__stack2) > 0:
+            self.__stack1.append(self.__stack2.pop())
+        return first
+
+    def empty(self) -> bool:
+        if not self.__stack1:
+            return True
+        else:
+            return False
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
