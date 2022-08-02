960ff75765e5d5bb341a3014c9f5bf36f6e4e38f
Mussie <90728686+Mussie7@users.noreply.github.com>
2022-08-02 16:37:29 +0300

    Added another way of implementation.
    The new approach uses the built-in heap

--- Heap Sort.py
+++ Heap Sort.py
@@ -56,3 +56,23 @@ class Solution:
         for i in range(1, n):
             arr[0], arr[-i] = arr[-i], arr[0]
             self.heapify(arr, n-i, 0)
+
+            
+# Using the built-in heap function
+import heapq
+
+class Solution:
+    #Heapify function to maintain heap property.
+    def heapify(self,arr, n, i):
+        temp = [heapq.heappop(arr) for j in range(n)]
+        arr.extend(temp)
+        temp = []
+
+    #Function to build a Heap from array.
+    def buildHeap(self,arr,n):
+        heapq.heapify(arr)
+    
+    #Function to sort an array using Heap Sort.    
+    def HeapSort(self, arr, n):
+        self.buildHeap(arr, n)
+        self.heapify(arr, n, 0)
