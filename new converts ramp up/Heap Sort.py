1b875b1db98c07d5a6e1d3aa1d4bcaf5b98945af
Mussie <90728686+Mussie7@users.noreply.github.com>
2022-08-02 16:40:31 +0300

    Create Heap Sort.py

---
+++ new converts ramp up/Heap Sort.py
@@ -0,0 +1,17 @@
+import heapq
+
+class Solution:
+    #Heapify function to maintain heap property.
+    def heapify(self,arr, n, i):
+        temp = [heapq.heappop(arr) for j in range(n)]
+        arr.extend(temp)
+        temp = []
+
+    #Function to build a Heap from array.
+    def buildHeap(self,arr,n):
+        heapq.heapify(arr)
+    
+    #Function to sort an array using Heap Sort.    
+    def HeapSort(self, arr, n):
+        self.buildHeap(arr, n)
+        self.heapify(arr, n, 0)
