3f5502c3bf96b60485e34a24cef8a51f3ff195fe
Mussie <90728686+Mussie7@users.noreply.github.com>
2022-08-03 19:00:53 +0300

    Create last stone weight.py

---
+++ new converts ramp up/last stone weight.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        for i in range(len(stones)):
+            stones[i] = -stones[i]
+        
+        heapq.heapify(stones)
+        while len(stones) > 1:
+            x, y = -heapq.heappop(stones), -heapq.heappop(stones)
+            if x - y:
+                heapq.heappush(stones, -(x-y))
+        
+        return -stones[0] if stones else 0
