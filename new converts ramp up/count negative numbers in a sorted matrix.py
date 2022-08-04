bb2060c36a5224256ef2bd7c5f20bd7b3be0b898
Mussie <90728686+Mussie7@users.noreply.github.com>
2022-08-04 22:49:00 +0300

    Create count negative numbers in a sorted matrix.py

---
+++ new converts ramp up/count negative numbers in a sorted matrix.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        n, m = len(grid[0]), len(grid)
+        ans = 0
+        for i in range(m):
+            start = 0
+            end = n - 1
+            best = n
+            while start <= end:
+                mid = start + (end-start)//2
+                if grid[i][mid] < 0:
+                    best = mid
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            ans += (n - best) * (m - i)
+            n = best
+            
+        return ans
